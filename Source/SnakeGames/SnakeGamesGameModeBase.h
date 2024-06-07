34cefde5955b84a30fd5fecc4f9392b158c3f145
Aghriamar <A1exXx88@inbox.ru>
2024-06-08 01:29:54 +0300

    Initial commit

---
+++ Source/SnakeGames/SnakeGamesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SnakeGamesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKEGAMES_API ASnakeGamesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
